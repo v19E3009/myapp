78ca42138c20963ea6751ac04cb1170b1c411519
Kenta Ogawa <v19e3009@oita-u.ac.jp>
2020-06-15 05:41:03 +0000

    Lesson07課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,10 @@ class PostsController < ApplicationController
     
     def index
         @posts = Post.all.order(created_at: 'desc')
+        render 'posts/index'
     end
+end
+
+def show
+    @post = Post.find(params[:id])
 end
