96f2ac2f0de42ef29cb27dccd5dacca6ab8efe4c
Kenta Ogawa <v19e3009@oita-u.ac.jp>
2020-06-29 07:24:45 +0000

    Lesson09課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,37 @@ class PostsController < ApplicationController
         @posts = Post.all.order(created_at: 'desc')
         render 'posts/index'
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        #render plain: params[:post].inspect
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            #render plain: @post.errors.inspect
+            render 'new'
+        end
+        
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
+    
+    
+    
 end
 
-def show
-    @post = Post.find(params[:id])
-end
-
-def new
-end
-
-def creat
-end
