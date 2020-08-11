47f0fd77cb78c0a94cb0f1b5095d10e8fa33cfe9
Kenta Ogawa <v19e3009@oita-u.ac.jp>
2020-08-11 02:17:00 +0000

    Lesson15課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,13 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        authorize @post
     end
 
     def create
         #render plain: params[:post].inspect
-        @post = Post.new(post_params)
+        @post = Post.new(post_params.merge({user_id: session[:user_id]}))
+        authorize @post
         if @post.save
             redirect_to posts_path
         else
@@ -27,10 +29,12 @@ class PostsController < ApplicationController
     
     def edit
         @post = Post.find(params[:id])
+        authorize @post
     end
     
     def update
         @post = Post.find(params[:id])
+        authorize @post
         if @post.update(post_params)
             redirect_to posts_path
         else
@@ -40,6 +44,7 @@ class PostsController < ApplicationController
     
     def destroy
         @post = Post.find(params[:id])
+        authorize @post
         @post.destroy
         redirect_to posts_path
     end
